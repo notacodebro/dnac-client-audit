21022e10e8bf36c2fb50c8cc283754bdc031b394
notacodebro <marc.nufrio@gmail.com>
2023-09-11 19:44:55 -0400

    initial commit, mvp and functional

---
+++ get_clients.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+import requests
+import json 
+import argparse
+import getpass
+import base64
+import urllib3
+
+
+BASE_URL = 'https://172.17.7.200'
+urllib3.disable_warnings()
+
+def authentication(material):
+    url = f'{BASE_URL}/dna/system/api/v1/auth/token'
+    headers = {"Content-Type":"application/json", "Accept":"application/json", "Authorization":f"Basic {material}"}
+    request = requests.post(url, headers=headers, verify=False)
+    return request
+
+def get_client(main_headers, mac=''):
+    headers = main_headers
+    if mac == '':
+        url = f'{BASE_URL}/dna/intent/api/v1/client-health'
+        meta = 'all'
+    else:
+        url = f'{BASE_URL}/dna/intent/api/v1/client-detail?macAddress={mac}'
+        meta = 'mac'
+    request = requests.get(url, headers=headers, verify=False)
+    return request, meta
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username', help='community string for snmp', required=True)
+    parser.add_argument('--mac', help='community string for snmp', required=False)
+    parser.add_argument('--interactive', help='community string for snmp', required=False)
+    args = parser.parse_args()
+    return args
+
+
+def printer(request, meta):
+    response = json.loads(request.text)
+    if meta == 'mac':
+        print('*'*64)
+        print(f"Client hostname: {response['detail']['hostName']}") 
+        print(f"Client IP: {response['detail']['hostIpV4']}") 
+        print(f"Client connectivity: {response['detail']['hostType']}") 
+        print(f"Client status: {response['detail']['connectionStatus']}") 
+        print(f"Client switch connection: {response['detail']['connectedDevice'][0]['name']}") 
+        print(f"Client switch port: {response['detail']['port']}\n") 
+        print('*'*64)
+        print('Client Health Score')
+        print('*'*64)
+        print(f"Client health score: {response['detail']['healthScore'][0]['score']}") 
+    else: 
+        print(json.dumps(json.loads(request.text), indent=3))
+
+def main():
+    args = parser()
+    password = getpass.getpass() 
+    material = base64.b64encode(f'{args.username}:{password}'.encode())
+    material = material.decode()
+    token = authentication(material)
+    token = token.json()['Token']
+    main_headers = {'X-Auth-Token': token, 'Content-Type': 'application/json'}
+    if args.mac:
+        request, meta = get_client(main_headers, args.mac)
+    else:
+       request, meta =  get_client(main_headers)
+    
+    printer(request, meta)
+main()
